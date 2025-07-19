939d7b7d8ff6bc56955b7a5ca290744e39092f16
Santhosh <santhoshkumarkarthikeyan5@gmail.com>
2025-07-19 22:40:59 +0530

    fix layout and add logic for validating two cards being open at a time

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,30 +1,52 @@
 import type { CardType } from "../types";
 import { Card } from "../components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+function getShuffledIndices(numSlots: number): number[] {
+  const arr: number[] = [];
+  for (let i: number = 0; i < numSlots / 2; i++) arr.push(i, i);
+  for (let i: number = numSlots - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
 
 export default function Board({ cards }: { cards: CardType[] }) {
-  function cardOpen(idx: number) {
-    console.log("Open cards: ",openCards)
-    if (idx in openCards)
-      return
-    if (openCards.length >= 2) setOpenCards([]);
-    else setOpenCards((curr) => [...curr, idx]);
-  }
   const [openCards, setOpenCards] = useState<number[]>([]);
+  const [cardLocations, setCardLocations] = useState<number[]>([]);
+  const [score, setScore] = useState<number>(0);
   const numSlots: number = 16;
-  const cardLocations: number[] = [];
-  for (let i = 0; i < numSlots / 2; i++) cardLocations.push(i, i);
-  for (let i = numSlots - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [cardLocations[i], cardLocations[j]] = [cardLocations[j], cardLocations[i]];
+  function cardOpen(idx: number) {
+    if (openCards.length >= 2) {
+      console.table(openCards[0] ? cards[openCards[0]] : -1);
+      console.table(openCards[1] ? cards[openCards[1]] : -1);
+      if (cards[openCards[0]].id === cards[openCards[1]].id)
+        setScore((score) => score + 12.5);
+      setOpenCards([idx]);
+    } else {
+      if (openCards.includes(idx)) return;
+      setOpenCards((curr) => [...curr, idx]);
+    }
   }
+  useEffect(() => setCardLocations(getShuffledIndices(numSlots)), []);
   const cardList = cardLocations.map((id, idx) => (
-    <Card key={idx} card={cards[id]} cardOpen={cardOpen} />
+    <Card key={idx} card={cards[id]} cardOpen={cardOpen} idx={idx} />
   ));
+  useEffect(() => console.log(openCards), [openCards]);
   return (
-    <main className="grid md:grid-cols-4 grid-rows-4 gap-y-5 gap-x-1">
-      {openCards}
-      {cardList}
+    <main className="flex flex-col items-around">
+      {[
+        openCards[0] ? cards[openCards[0]].id : -1,
+        openCards[1] ? cards[openCards[1]].id : -1,
+      ].join(" , ")}
+      <header className="bg-zinc-300/30 border border-zinc-900 p-5 m-5 rounded-sm flex justify-around items-center">
+        <h3 className="text-3xl">Memory Test</h3>
+        <p className="">Score: {score}</p>
+      </header>
+      <section className="grid md:grid-cols-8 grid-rows-2 gap-10 m-5 self-center">
+        {cardList}
+      </section>
     </main>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,9 @@
 import type { CardType } from "../types";
 import { idToEmoji } from "../utilities";
 
-export default function Card({ card, cardOpen}: { card: CardType, cardOpen: (idx: number) => void }) {
+export default function Card({ card, cardOpen, idx}: { card: CardType, cardOpen: (idx: number) => void , idx: number}) {
   return (
-    <main onClick={() => cardOpen(card.id)} className="aspect-[2/3] w-36 py-5 bg-gray-200/20 flex flex-col justify-end items-center space-y-15 rounded-md border border-zinc-900">
+    <main onClick={() => cardOpen(idx)} className="cursor-pointer aspect-[2/3] w-36 py-5 bg-gray-200/20 flex flex-col justify-end items-center space-y-15 rounded-md border border-zinc-900">
       <section className="text-5xl">
         {card.id}
         {card.id >= 0 && card.id <= idToEmoji.length ? card.emoji : "😐"}
