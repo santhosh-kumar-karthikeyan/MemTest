aed5fc55416cf0075a580d3e42f2192ee5d5ca16
Santhosh <santhoshkumarkarthikeyan5@gmail.com>
2025-07-20 18:16:36 +0530

    add functionality for flipping exactly two cards at a time

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,7 @@
 import type { CardType } from "../types";
 import { Card } from "../components";
 import { useEffect, useState } from "react";
+import { RiTimerFlashFill } from "react-icons/ri";
 
 function getShuffledIndices(numSlots: number): number[] {
   const arr: number[] = [];
@@ -15,34 +16,43 @@ function getShuffledIndices(numSlots: number): number[] {
 export default function Board({ cards }: { cards: CardType[] }) {
   const [openCards, setOpenCards] = useState<number[]>([]);
   const [cardLocations, setCardLocations] = useState<number[]>([]);
+  const [time, setTime] = useState<number>(60);
+  const [foundCards, setFoundCards] = useState<number[]>([]);
   const [score, setScore] = useState<number>(0);
   const numSlots: number = 16;
   function cardOpen(idx: number) {
     if (openCards.length >= 2) {
-      console.table(openCards[0] ? cards[openCards[0]] : -1);
-      console.table(openCards[1] ? cards[openCards[1]] : -1);
-      if (cards[openCards[0]].id === cards[openCards[1]].id)
+      if (
+        cards[cardLocations[openCards[0]]].id ===
+        cards[cardLocations[openCards[1]]].id
+      )
         setScore((score) => score + 12.5);
       setOpenCards([idx]);
     } else {
-      if (openCards.includes(idx)) return;
       setOpenCards((curr) => [...curr, idx]);
     }
   }
   useEffect(() => setCardLocations(getShuffledIndices(numSlots)), []);
   const cardList = cardLocations.map((id, idx) => (
-    <Card key={idx} card={cards[id]} cardOpen={cardOpen} idx={idx} />
+    <Card
+      key={idx}
+      card={cards[id]}
+      cardOpen={cardOpen}
+      idx={idx}
+      openCards={openCards}
+    />
   ));
   useEffect(() => console.log(openCards), [openCards]);
   return (
     <main className="flex flex-col items-around">
-      {[
-        openCards[0] ? cards[openCards[0]].id : -1,
-        openCards[1] ? cards[openCards[1]].id : -1,
-      ].join(" , ")}
+      {cards.map((card) => card.id)}
+      <br />
+      {openCards.join(",")}
       <header className="bg-zinc-300/30 border border-zinc-900 p-5 m-5 rounded-sm flex justify-around items-center">
-        <h3 className="text-3xl">Memory Test</h3>
-        <p className="">Score: {score}</p>
+        <h4 className="text-4xl flex justify-center items-center space-5">
+          <RiTimerFlashFill /> Time left: {time}
+        </h4>
+        <p className="text-xl">Score: {score}</p>
       </header>
       <section className="grid md:grid-cols-8 grid-rows-2 gap-10 m-5 self-center">
         {cardList}
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,37 @@
 import type { CardType } from "../types";
 import { idToEmoji } from "../utilities";
 
-export default function Card({ card, cardOpen, idx}: { card: CardType, cardOpen: (idx: number) => void , idx: number}) {
+export default function Card({
+  card,
+  cardOpen,
+  idx,
+  openCards,
+}: {
+  card: CardType;
+  cardOpen: (idx: number) => void;
+  idx: number;
+  openCards: number[];
+}) {
+  function handleClick(currIdx: number) {
+    if (!openCards.includes(currIdx)) {
+      cardOpen(currIdx);
+    }
+  }
   return (
-    <main onClick={() => cardOpen(idx)} className="cursor-pointer aspect-[2/3] w-36 py-5 bg-gray-200/20 flex flex-col justify-end items-center space-y-15 rounded-md border border-zinc-900">
-      <section className="text-5xl">
-        {card.id}
-        {card.id >= 0 && card.id <= idToEmoji.length ? card.emoji : "😐"}
-      </section>
-      <p>{card.name}</p>
+    <main
+      onClick={() => handleClick(idx)}
+      className="cursor-pointer aspect-[2/3] w-36 py-5 bg-gray-200/20 flex flex-col justify-end items-center space-y-15 rounded-md border border-zinc-900">
+      {openCards.includes(idx) ? (
+        <>
+          <section className="text-5xl">
+            {card.id}
+            {card.id >= 0 && card.id <= idToEmoji.length ? card.emoji : "😐"}
+          </section>
+          <p>{card.name}</p>
+        </>
+      ) : (
+        <section>Click to reveal</section>
+      )}
     </main>
   );
 }
